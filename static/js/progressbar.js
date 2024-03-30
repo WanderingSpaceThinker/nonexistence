90b720d4574ed49d9a9a6d18d4cc8c0f37d3e855
WST <wanderingspacetraveler@gmail.com>
2024-03-30 14:00:04 +0100

    feat: add article scroll progress bar

---
+++ static/js/progressbar.js
@@ -0,0 +1,29 @@
+
+
+
+window.onload = function() {
+  const container = document.querySelector('.progress_container');
+  const progressBar = document.querySelector('.progress-bar');
+  const totalLength = progressBar.getTotalLength();
+  
+  progressBar.style.strokeDasharray = totalLength;
+  progressBar.style.strokeDashoffset = totalLength;
+  container.style.visibility = 'hidden';
+  container.style.opacity = 0;
+
+  window.addEventListener('scroll', () => {
+    setProgress(container, progressBar, totalLength);
+  });
+}
+
+function setProgress(container, progressBar, totalLength) {
+  const clientHeight = document.documentElement.clientHeight;
+  const scrollHeight = document.documentElement.scrollHeight;
+  const scrollTop = document.documentElement.scrollTop;
+  
+  const percentage = scrollTop / (scrollHeight - clientHeight);
+  progressBar.style.strokeDashoffset = totalLength - totalLength * percentage;
+
+  container.style.visibility = 'visible';
+  container.style.opacity = 1;
+}
