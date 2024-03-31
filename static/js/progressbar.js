7b2e972c38880552c63dacfe7e218456b5075d60
WST <wanderingspacetraveler@gmail.com>
2024-03-31 21:29:26 +0200

    fix: progressbar error on non-article pages

--- static/js/progressbar.js
+++ static/js/progressbar.js
@@ -4,16 +4,19 @@
 window.onload = function() {
   const container = document.querySelector('.progress_container');
   const progressBar = document.querySelector('.progress-bar');
-  const totalLength = progressBar.getTotalLength();
-  
-  progressBar.style.strokeDasharray = totalLength;
-  progressBar.style.strokeDashoffset = totalLength;
-  container.style.visibility = 'hidden';
-  container.style.opacity = 0;
 
-  window.addEventListener('scroll', () => {
-    setProgress(container, progressBar, totalLength);
-  });
+  if (progressBar) {
+    const totalLength = progressBar.getTotalLength();
+
+    progressBar.style.strokeDasharray = totalLength;
+    progressBar.style.strokeDashoffset = totalLength;
+    container.style.visibility = 'hidden';
+    container.style.opacity = 0;
+
+    window.addEventListener('scroll', () => {
+      setProgress(container, progressBar, totalLength);
+    });
+  }
 }
 
 function setProgress(container, progressBar, totalLength) {
